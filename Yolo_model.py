ad09c940a57faa4c2973c52c273b2a4d33e7041e
Yossefmohammed <ypssefmohammedahmed@gmail.com>
2025-05-17 14:49:34 +0300

    computer vision project

---
+++ Yolo_model.py
@@ -0,0 +1,25 @@
+# https://pysource.com/2023/02/21/yolo-v8-segmentation
+from ultralytics import YOLO
+import numpy as np
+
+
+class YOLOSEG:
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
+
+    def detect(self, img):
+        height, width, channels = img.shape
+
+        results = self.model.predict(source=img.copy(), save=False, save_txt=False)
+        result = results[0]
+
+        segmentation_contours_idx = []
+        if result.masks is not None and len(result.masks.xy) > 0:
+            for seg in result.masks.xy:
+                segment = np.array(seg, dtype=np.int32)
+                segmentation_contours_idx.append(segment)
+
+        bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
+        class_ids = np.array(result.boxes.cls.cpu(), dtype="int")
+        scores = np.array(result.boxes.conf.cpu(), dtype="float").round(2)
+        return bboxes, class_ids, segmentation_contours_idx, scores
